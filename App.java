5311853481df54140f71420ec227a06ba2eb1484
GustavoDias <gustavoalvesdiaz08@gmail.com>
2021-10-04 19:40:21 -0300

    1 - adição de mais clientes ao Array; 2 - adição do método toUpperCase no switch, para que o exit seja executado independente do formato que for digitado

--- App.java
+++ App.java
@@ -12,19 +12,22 @@ public class App {
         Scanner entrada = new Scanner(System.in);
 
         lista.add(new Cliente("Alessandra", "Feminino", "São Paulo", "ale@gmail.com", "21", "(11) 4422-5594"));
+        lista.add(new Cliente("Bianca", "Feminino", "Minas Gerais", "bia@gmail.com", "18", "(31) 5892-3394"));
         lista.add(new Cliente("Gabriel", "Masculino", "Santana de Parnaiba", "bielzinho@gmail.com", "19", "(11) 94452-8533"));
         lista.add(new Cliente("Gustavo", "Masculino", "São Paulo", "gustavinho@gmail.com", "21", "(11) 3353-9922"));
-        lista.add(new Cliente("Julia", "Feminino", "São Paulo", "julinhabebedeira@gmail.com", "19", "(11)5566-0922"));
         lista.add(new Cliente("Joao", "Masculino", "Bahia", "joaozinmatador@gmail.com", "30", "(71) 5594-7825"));
+        lista.add(new Cliente("Julia", "Feminino", "São Paulo", "julinhabebedeira@gmail.com", "19", "(11)5566-0922"));
         lista.add(new Cliente("Leandro", "Masculino", "Santa Catarina", "leandrozindosroles@gmail.com", "25", "(47) 9543-1478"));
+        lista.add(new Cliente("Mauro", "Masculino", "Espírito Santo", "mauro2021@gmail.com", "40", "(27) 1043-1478"));
         lista.add(new Cliente("Pedro", "Masculino", "São Paulo", "pedringames@gmail.com", "11", "(11) 4002-8922"));
+        lista.add(new Cliente("Ulices", "Masculino", "Goiás", "ulicestrader@gmail.com", "35", "(62) 4802-9934"));
 
         while (!nome.equalsIgnoreCase("exit")) {
             System.out.print("Digite o nome procurado: ");
             nome = entrada.nextLine();
 
-            switch (nome) {
-                case "exit":
+            switch (nome.toUpperCase()) {
+                case "EXIT":
                     return;
 
                 default:
